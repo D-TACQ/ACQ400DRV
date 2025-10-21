13adddd09489e52bc880d0b1c6fbce6b28db9788
Peter Milne <peter.milne@d-tacq.com>
2025-10-21 15:52:53 +0100

    just because it's TCL it doesn't _have_ to be obscure.. .. add symbols for ctypes (carrier types)  On branch main2 	new file:   CARE/acq400_mod_id.tcl 	modified:   CARE/acq400t_test

---
+++ CARE/acq400_mod_id.tcl
@@ -0,0 +1,20 @@
+
+# from acq400_mod_id.h
+
+# carrier mtypes values, they happen to be hex numbers, but used as strings
+
+set MOD_ID_ACQ2006SC	80
+set MOD_ID_ACQ1001SC	81
+set MOD_ID_ACQ2106SC	82
+set MOD_ID_KMCU		83
+set MOD_ID_KMCU30	84
+set MOD_ID_CPSC2	85
+set MOD_ID_Z7IO		86
+set MOD_ID_ACQ2206SC	87
+set MOD_ID_ACQ1102SC	88
+
+set MOD_ID_MTCA_ADAP	fc
+
+
+
+#endif /* ACQ400_MOD_ID_H_ */
