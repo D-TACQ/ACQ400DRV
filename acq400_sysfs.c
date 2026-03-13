8e834ac90ebfc9b804eb03cc9c65cd04fd20e675
Craig Hay <craig.hay@d-tacq.co.uk>
2026-03-13 13:55:57 +0000

    Add adc resolution for acq428

--- acq400_sysfs.c
+++ acq400_sysfs.c
@@ -2386,12 +2386,14 @@ static const struct attribute *acq426_attrs[] = {
 // TODO: what do we need to do with this for acq428?
 MAKE_BITS(idelay_en, ADC_CTRL, MAKE_BITS_FROM_MASK, ACQ428_ADC_CTRL_IDELAY_EN);
 MAKE_BITS(sync_en,  ADC_CTRL, MAKE_BITS_FROM_MASK, ACQ428_ADC_CTRL_SYNC_EN);
+MAKE_BITS(adc_resolution, ADC_CTRL, MAKE_BITS_FROM_MASK, ACQ428_ADC_CTRL_RESOLUTION_18B);
 MAKE_BITS(xrm_d37_mode, ACQ428_BANK, MAKE_BITS_FROM_MASK, ACQ428_BANK_XRM_D37_MODE);
 
 static const struct attribute *acq428_attrs[] = {
 	&dev_attr_idelay_en.attr,
 	&dev_attr_sync_en.attr,
         &dev_attr_xrm_d37_mode.attr,
+        &dev_attr_adc_resolution.attr,
 	NULL
 };
 SCOUNT_KNOB(CLK_EXT, 	ACQ2006_CLK_COUNT(0));
