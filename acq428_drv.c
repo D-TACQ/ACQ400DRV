decd36e999587e734501a60ee354a21ace637a99
Craig Hay <craig.hay@d-tacq.co.uk>
2026-03-20 14:41:53 +0000

    Back out the spi hook in acq428_drv.c

--- acq428_drv.c
+++ acq428_drv.c
@@ -50,8 +50,6 @@ static int ndev;
 #define PGA_ADDR_2      0x23
 #define N_PGA_GPIO 24
 
-extern void acq480_hook_spi_cb(void);
-
 static struct i2c_client* new_device(
 		struct i2c_adapter *adap,
 		const char* name, unsigned short addr, int gpio_base)
@@ -70,7 +68,6 @@ static void __init acq428_init_site(int site)
 {
 	int ch = site+1;
 	int gpio_base = acq428_gpio_base + ndev * N_PGA_GPIO;
-        acq480_hook_spi_cb();
 
 	i2c_adap[site] = i2c_get_adapter(ch);
 
