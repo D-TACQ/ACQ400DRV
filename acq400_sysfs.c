3bf3989539ba7128b96011f753e7e5f7cafdb503
Craig Hay <craig.hay@d-tacq.co.uk>
2026-03-31 12:36:31 +0100

    Make adc clock select knob available for acq428

--- acq400_sysfs.c
+++ acq400_sysfs.c
@@ -2398,6 +2398,7 @@ static const struct attribute *acq428_attrs[] = {
 	&dev_attr_adc_18b.attr,
 	&dev_attr_adc_conv_time.attr,
         &dev_attr_acq428_calib_debug.attr,
+        &dev_attr_adc_clk_sel.attr,
 	NULL
 };
 SCOUNT_KNOB(CLK_EXT, 	ACQ2006_CLK_COUNT(0));
