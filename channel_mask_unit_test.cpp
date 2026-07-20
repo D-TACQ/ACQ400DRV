e43889eb4f22acd084c0ca541cdb31474b904a63
Peter Milne <peter.milne@d-tacq.com>
2026-07-20 12:45:21 +0100

    Full unit test documented as below:
    (base) pgm@hoy6:~/PROJECTS/ACQ400/ACQ400DRV$ ChannelMaskALWAYS_BITSET=0 ./channel_mask_unit_test.x86
    
    channel_mask_tester
    start:0 length:64
    
    channel_mask_tester 8
    start:0 length:8
    
    channel_mask_tester  two numbers, return start,len
    start:0 length:64
    
    channel_mask_tester 9,16
    start:9 length:16
    
    channel_mask_tester 0x355555555
    1010101010101010101010101010101011
    channel_mask_tester 0x3aaaaaaaa
    0101010101010101010101010101010111
    channel_mask_tester 0x3f00000ff
    1111111100000000000000000000111111
    channel_mask_tester 0xFFFF00000000FFFF
    1111111111111111000000000000000000000000000000001111111111111111
    
    channel_mask_tester 0b1111000011
    1100001111
    
    channel_mask_tester CH9,10,11,12,13,14,15,16
    0000000011111111
    channel_mask_tester CH9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
    0000000011111111111111111
    channel_mask_tester CH1,2,3,4,5,6,7,8,33,34,35,36
    111111110000000000000000000000001111
    channel_mask_tester a.b.c
    start:0 length:64
    
    channel_mask_tester x1
    start:0 length:64
    
    channel_mask_tester !
    start:0 length:64
    
    channel_mask_tester @123
    start:0 length:64
    
    channel_mask_tester 1,3,9
    101000001
    
    (base) pgm@hoy6:~/PROJECTS/ACQ400/ACQ400DRV$ ChannelMaskALWAYS_BITSET=1 ./channel_mask_unit_test.x86
    
    channel_mask_tester
    1111111111111111111111111111111111111111111111111111111111111111
    channel_mask_tester 8
    111111111
    channel_mask_tester  two numbers, return start,len
    1111111111111111111111111111111111111111111111111111111111111111
    channel_mask_tester 9,16
    00000000011111111111111111
    
    channel_mask_tester 0x355555555
    1010101010101010101010101010101011
    channel_mask_tester 0x3aaaaaaaa
    0101010101010101010101010101010111
    channel_mask_tester 0x3f00000ff
    1111111100000000000000000000111111
    channel_mask_tester 0xFFFF00000000FFFF
    1111111111111111000000000000000000000000000000001111111111111111
    
    channel_mask_tester 0b1111000011
    1100001111
    
    channel_mask_tester CH9,10,11,12,13,14,15,16
    0000000011111111
    channel_mask_tester CH9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
    0000000011111111111111111
    channel_mask_tester CH1,2,3,4,5,6,7,8,33,34,35,36
    111111110000000000000000000000001111
    channel_mask_tester a.b.c
    1111111111111111111111111111111111111111111111111111111111111111
    channel_mask_tester x1
    1111111111111111111111111111111111111111111111111111111111111111
    channel_mask_tester !
    1111111111111111111111111111111111111111111111111111111111111111
    channel_mask_tester @123
    1111111111111111111111111111111111111111111111111111111111111111
    
    channel_mask_tester 1,3,9
    101000001
    
     On branch ChannelMaskUpgrade
    	modified:   channel_mask_unit_test.cpp

--- channel_mask_unit_test.cpp
+++ channel_mask_unit_test.cpp
@@ -8,17 +8,58 @@
 #include "ChannelMask.h"
 #include "Env.h"
 
+const char* unit_tests[] = {
+"my name",    // argv0 is the program name, not evaluated..
+"#CASE1 (see ChannelMask.h)\n",
+"# blank.. return full house",
+"",
+"# single number, return 0..len",
+"8",
+" two numbers, return start,len",
+"9,16",
+"#CASE2\n",
+"0x355555555",
+"0x3aaaaaaaa",
+"0x3f00000ff",
+"#  ChannelMaskNCHAN=128 for best result",
+"0xFFFF00000000FFFF",
+"#CASE3\n",
+"0b1111000011",
+"#CASE4\n",
+"CH9,10,11,12,13,14,15,16",
+"# .. now try again ChannelMaskALWAYS_BITSET and compare with 9,16",
+"# .. correct replacement for CASE#1 9,16:",
+"CH9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25",
+"CH1,2,3,4,5,6,7,8,33,34,35,36",
+"# some garbage input, results in full house",
+"a.b.c",
+"x1",
+"!",
+"@123",
+"# if the CASE1 contiguous list has > 2 elements, assume channel numbers like CASE4\n",
+"1,3,9"
+};
+const int ntests = sizeof(unit_tests)/sizeof(char*);
 
-int main(int argc, char* argv[]){
+int main(int argc, const char* argv[]){
 	ChannelMask::max_chan = Env::getenv("ChannelMaskNCHAN", 64);
 	ChannelMask::is_always_a_bitset = Env::getenv("ChannelMaskALWAYS_BITSET", 0);
+
+	if (argc == 1){
+		argc = ntests;
+		argv = unit_tests;
+	}
 
 	ChannelMask *cm;
 	for (int ii = 1; ii < argc; ++ii){
 		const char* def = argv[ii];
-		printf("channel_mask_tester %s\n", def);
-		cm = ChannelMask::factory(def);
-		printf("%s\n", cm->to_string().c_str());
+		if (def[0] == '#'){
+			printf("%s\n", def);
+		}else{
+			printf("channel_mask_tester %s\n", def);
+			cm = ChannelMask::factory(def);
+			printf("%s\n", cm->to_string().c_str());
+		}
 	}
 	return 0;
 }
